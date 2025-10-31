1abd5e6a85ce98e6eb361b6a762bf6382bcc051f
Colocegi <juanicolocegi@gmail.com>
2025-10-31 11:12:32 -0300

    IOpBan, Serv, Trans

--- TPO/src/Deposito.java
+++ TPO/src/Deposito.java
@@ -1,2 +1,31 @@
-public class Deposito {
+public class Deposito extends Transaccion {
+
+    public Depositar(String idTransaccion, double montoDeposito, String fecha) {
+        super(idTransaccion, montoDeposito, fecha);
+    }
+
+    @Override
+    protected void operar() {
+        // Método abstracto implementado
+        // La lógica principal está en ejecutar()
+    }
+
+    @Override
+    public void ejecutar(Cuenta cuenta) {
+        if (this.monto <= 0) {
+            System.out.println("Error: El monto a depositar debe ser mayor a 0");
+            return;
+        }
+
+        cuenta.acreditar(this.monto);
+        cuenta.listaTransacciones.add(this);
+
+        System.out.println("Depósito exitoso de $" + this.monto);
+        System.out.println("Nuevo saldo: $" + cuenta.getSaldo());
+    }
+
+    @Override
+    public String toString() {
+        return "Depositar: " + super.toString();
+    }
 }
